2797dbd231600e10f7d621444c3419fa5a0f718f
jerome813 <johnjeromeperez@gmail.com>
2022-07-27 17:22:55 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,64 +1,65 @@
 const path = require('path');
-const express = require('express')
-const colors = require('colors')
-const dotenv = require('dotenv').config()
-const { errorHandler } = require('./middleware/errorMiddleware')
-const connectDB = require('./config/db')
-const cors = require('cors')
-const port = process.env.PORT || 5000
-const socket = require("socket.io");
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const socket = require('socket.io');
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-app.use(cors())
+app.use(cors());
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/users', require('./routes/userRoutes'))
-app.use('/api/posts', require('./routes/postRoutes'))
-app.use('/api/comments', require('./routes/commentRoutes'))
-app.use('/api/friends', require('./routes/friendRoutes'))
-app.use('/api/messages', require('./routes/messageRoutes'))
+app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/posts', require('./routes/postRoutes'));
+app.use('/api/comments', require('./routes/commentRoutes'));
+app.use('/api/friends', require('./routes/friendRoutes'));
+app.use('/api/messages', require('./routes/messageRoutes'));
 
 // Serve frontend
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')))
+	app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-  app.get('*', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-    )
-  )
+	app.get('*', (req, res) =>
+		res.sendFile(
+			path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+		)
+	);
 } else {
-  app.get('/', (req, res) => res.send('Please set to production'))
+	app.get('/', (req, res) => res.send('Please set to production'));
 }
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-const server = app.listen(port, () => console.log(`Server started on port ${port}`))
-
+const server = app.listen(port, () =>
+	console.log(`Server started on port ${port}`)
+);
 
 const io = socket(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    credentials: true,
-  },
+	cors: {
+		origin: 'http://localhost:3000',
+		credentials: true,
+	},
 });
 
 global.onlineUsers = new Map();
-io.on("connection", (socket) => {
-  global.chatSocket = socket;
-  socket.on("add-user", (userId) => {
-    onlineUsers.set(userId, socket.id);
-  });
+io.on('connection', (socket) => {
+	global.chatSocket = socket;
+	socket.on('add-user', (userId) => {
+		onlineUsers.set(userId, socket.id);
+	});
 
-  socket.on("send-msg", (data) => {
-    const sendUserSocket = onlineUsers.get(data.to);
-    if (sendUserSocket) {
-      socket.to(sendUserSocket).emit("msg-recieve", data);
-    }
-  });
+	socket.on('send-msg', (data) => {
+		const sendUserSocket = onlineUsers.get(data.to);
+		if (sendUserSocket) {
+			socket.to(sendUserSocket).emit('msg-recieve', data);
+		}
+	});
 });
