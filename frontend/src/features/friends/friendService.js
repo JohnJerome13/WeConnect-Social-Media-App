91d29a45c0f677b2c77dcc30748452797dcbd945
jerome813 <johnjeromeperez@gmail.com>
2022-07-28 05:09:05 +0800

    API url update

--- frontend/src/features/friends/friendService.js
+++ frontend/src/features/friends/friendService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://weconnect-mern-app.herokuapp.com/api/users/';
+const API_URL = 'https://weconnect-mern-app.herokuapp.com/api/friends/';
 
 const config = (token) => {
 	return {
