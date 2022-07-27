29c9c190fcacf9c236f6751fa122bab38dc048b9
jerome813 <johnjeromeperez@gmail.com>
2022-07-27 18:09:33 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === 'production') {
 
 	app.get('*', (req, res) =>
 		res.sendFile(
-			path.resolve(__dirname, '../', 'frontend', '.next', 'index.html')
+			path.resolve(__dirname, '../', 'frontend', '.next')
 		)
 	);
 } else {
